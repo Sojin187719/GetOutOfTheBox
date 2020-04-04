cc62ba29db4fca3e4557d716574950a0cda33fcc
stefanjij <stefanbbjij@yahoo.fr>
2020-04-04 23:02:35 +0200

    fusion niko

--- js/content-script.js
+++ js/content-script.js
@@ -5,6 +5,7 @@ var localURL = location;
 var interval = 1000;
 var id = 0;
 
+createCursor("Me",0,0);
 
 var firebaseConfig = {
     apiKey: "AIzaSyBl6wXfdx33ui2MJ5tnTJUkVgmknleZpUU",
@@ -20,38 +21,34 @@ var firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const database = app.database();
  
-const div = document.createElement('div');
-        div.id = "cursor";
-        div.innerHTML = 'lala';
-        div.style.position = 'fixed';
-        document.body.appendChild(div);
-        
+
 
 const ref = database.ref('data/olivier');
 ref.on('child_changed', function(snapshot, data) {
-  console.log(`olivier ${snapshot.key}: ${snapshot.val()}`);
+    console.log(`olivier ${snapshot.key}: ${snapshot.val()}`);
 
-  if (snapshot.key === 'x') {
-    document.querySelector('#cursor').style.left = snapshot.val() + 'px';
-} else {
-    document.querySelector('#cursor').style.top = snapshot.val() + 'px';
-}
-
+    if (snapshot.key === 'x') {
+        document.querySelector('#cursor').style.left = snapshot.val() + 'px';
+    }else {
+        document.querySelector('#cursor').style.top = snapshot.val() + 'px';
+    }
 });
 
 
 
 const coord =  async function(e){
-    if(isSleeping) return;
-    isSleeping = true
-    var x = e.pageX;
-    var y = e.pageY;
-    if(getCursor("Me")==null) createCursor("Me",x,y);
-    getCursor("Me").x=x;
-    getCursor("Me").y=y;
-    moveCursor(getCursor("Me"))
-    await sleep(interval);
-    isSleeping = false;
+	if(isSleeping) return;
+	isSleeping = true
+	var cursor = getCursor("Me");
+	var x = e.pageX;
+	var y = e.pageY;
+	cursor.oldX=cursor.x;
+	cursor.oldY=cursor.y;
+	cursor.x=x;
+	cursor.y=y;
+	moveCursor(cursor)
+	await sleep(interval);
+	isSleeping = false;
 };
 
 function sleep(ms) {
@@ -67,54 +64,77 @@ function getCursor(id) {
     return null;
 }
 function createCursor(cursorId, cursorX, cursorY){
-    var mainDiv =document.createElement("div");
-    mainDiv.name=cursorId;
-    mainDiv.style.position="absolute";
-    mainDiv.style.pointerEvents="none";
-    var img = document.createElement("img");
-    img.src="https://openclipart.org/download/222074/White-Pixel-Mouse-Cursor-Arow-Fixed.svg";
-    img.style.width="9px";
-    img.style.height="auto";
-    var p = document.createElement("p");
-    p.style.fontSize ="13px";
-    p.style.position ="absolute";
-    p.style.top ="0%";
-    p.style.left ="75%";
-    var node = document.createTextNode(cursorId);
-    p.appendChild(node);
-    mainDiv.appendChild(img);
-    mainDiv.appendChild(p);
-    var cursor = {id: cursorId, x: cursorX, y: cursorY,docElement: mainDiv};
-    cursorList.push(cursor);
-    document.body.appendChild(mainDiv);
+	var mainDiv =document.createElement("div");
+	mainDiv.name=cursorId;
+	mainDiv.style.position="absolute";
+	mainDiv.style.pointerEvents="none";
+	mainDiv.style.zIndex="2147483647";
+	var img = document.createElement("img");
+	img.src="https://openclipart.org/download/222074/White-Pixel-Mouse-Cursor-Arow-Fixed.svg";
+	img.style.width="9px";
+	img.style.height="auto";
+	var p = document.createElement("p");
+	p.style.fontSize ="13px";
+	p.style.position ="absolute";
+	p.style.top ="0%";
+	p.style.left ="75%";
+	var node = document.createTextNode(cursorId);
+	p.appendChild(node);
+	mainDiv.appendChild(img);
+	mainDiv.appendChild(p);
+	var cursor = {id: cursorId, x: cursorX, y: cursorY, oldX: 0, oldY: 0,docElement: mainDiv};
+	cursorList.push(cursor);
+	document.body.appendChild(mainDiv);
 }
+   
 
 function deleteCursor(cursorId){
-    var cursor = getCursor(cursorId);
-    cursorList.splice(cursorList.indexOf(cursor), 1);
-    document.body.removeChild(cursor.docElement);
+	var cursor = getCursor(cursorId);
+	cursorList.splice(cursorList.indexOf(cursor), 1);
+	document.body.removeChild(cursor.docElement);
 }
 
 
-function moveCursor(){
-    for(i = 0 ; i<cursorList.length ; i++){
-        cursor = cursorList[i];
-        console.log("id : "+cursor.id+" | x: "+cursor.x+" | y: "+cursor.y);
-        database.ref('data/olivier').set({
-            cursorId: cursor.id,
-            x: cursor.x,
-            y: cursor.y
-        }, function(error) {
-            if (error) {
-                console.log(`arf: ${error}`);
-            } else {
-                console.log('what!?');
-            }
-        });
-        cursor.docElement.style.top = cursor.y-8;
-        cursor.docElement.style.left = cursor.x-8;
-    }
+function moveCursor(cursor){
+	console.log("x: "+cursor.x+" | y: "+cursor.y);
+	var diffX = cursor.x-cursor.oldX;
+	var diffY = cursor.y-cursor.oldY;
+	var nbDepl = Math.abs(diffX)>Math.abs(diffY) ? Math.abs(diffX):Math.abs(diffY);
+	var count=1;
+	var speed = interval/nbDepl;
+	
+	if(speed<10){
+		nbDepl/=12.5;
+		speed=10;
+	}
+	
+	var distanceY=diffY/nbDepl;
+	var distanceX=diffX/nbDepl;
+	
+	var id = setInterval(frame, speed);
+
+	function frame() {
+		if (count>=nbDepl) {
+			clearInterval(id);
+		} else {
+			count++;
+			cursor.docElement.style.top = cursor.oldY+distanceY*count+"px";
+			cursor.docElement.style.left =  cursor.oldX+distanceX*count+"px";
+		}
+	}
 }
 
 
-document.addEventListener('mousemove', coord, true);
+document.addEventListener('mousemove', coord, true); 
+
+// database.ref('data/olivier').set({
+//             cursorId: cursor.id,
+//             x: cursor.x,
+//             y: cursor.y
+//         }, function(error) {
+//             if (error) {
+//                 console.log(`arf: ${error}`);
+//             } else {
+//                 console.log('what!?');
+//             }
+//         });
